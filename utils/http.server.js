1de48100026c378bda8378f5b9bdcfe1a7af95a1
abdou6666 <abdousfayhi12@gmail.com>
2023-05-10 19:22:09 +0100

    removed crendentials

--- utils/http.server.js
+++ utils/http.server.js
@@ -57,7 +57,8 @@ class HttpServer {
 
             app.use(express.json())
             app.use(cors({
-                origin: ['http://localhost:3000', 'http://localhost:3001', 'https://pethub-front.vercel.app', 'https://pethub-front-git-main-pethub.vercel.app', 'https://pethub-front-6ykj48q09-pethub.vercel.app']
+                origin: ['http://localhost:3000', 'http://localhost:3001', 'https://pethub-front.vercel.app',
+                    'https://pethub-front-git-main-pethub.vercel.app', 'https://pethub-front-6ykj48q09-pethub.vercel.app']
             }))
             app.use(errorHandler);
 
@@ -102,12 +103,15 @@ class HttpServer {
 
             const io = new Server(this.server, {
                 cors: {
-                    origin: ['http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5173/', 'http://localhost:5173', 'http://127.0.0.1:5174/', 'https://localhost:5173', 'https://127.0.0.1:5173', 'http://127.0.0.1:5500/', 'http://127.0.0.1:5173/',
-                        'https://pethub-front.vercel.app', 'https://pethub-front-git-main-pethub.vercel.app', 'https://pethub-front-6ykj48q09-pethub.vercel.app'
+                    origin: ['http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5173/', 'http://localhost:5173',
+                        'http://127.0.0.1:5174/', 'https://localhost:5173', 'https://127.0.0.1:5173', 'http://127.0.0.1:5500/',
+                        'http://127.0.0.1:5173/',
+                        'https://pethub-front.vercel.app', 'https://pethub-front-git-main-pethub.vercel.app',
+                        'https://pethub-front-6ykj48q09-pethub.vercel.app'
                     ]
                 },
                 cookie: true,
-                credentials: true
+                // credentials: true
             });
             const loggedInUsers = LoggedInUsers.getInstance();
             io.on("connection", (socket) => {
